c3ba42e5493ab11de615604d30a06733dfbf9d57
Katiana Xavier <katianaxavierb@gmail.com>
2023-03-27 12:58:03 -0300

    Security do Loja Games

--- Loja-Games/lojagames/src/main/java/com/generation/lojagames/model/Produto.java
+++ Loja-Games/lojagames/src/main/java/com/generation/lojagames/model/Produto.java
@@ -47,6 +47,10 @@ public class Produto {
 	@JsonIgnoreProperties ("produto")
 	private Categoria categoria;
 	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Usuario usuario;
+	
 	public Long getId() {
 		return id;
 	}
@@ -101,6 +105,14 @@ public class Produto {
 
 	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 
 }
